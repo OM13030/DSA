b44e4fd3f29aa3edf262381487d18811a8fcc8e6
om gajera <omgajera8@gmail.com>
2026-05-09 01:21:33 +0530

    Add StarTriangleReverse class for generating a reverse star triangle pattern

---
+++ 4.patterns/StarTriangleReverse.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class StarTriangleReverse{
+    public static void main(String[] args) {
+         Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of rows :  ");
+        int m = sc.nextInt();
+                
+        for(int i = 1; i<= m;i++){
+            for(int j = 1; j<= m;j++){
+               if((i+j)<=m){
+                System.out.print(" ");
+               }
+               else{
+                System.out.print(j);
+               }
+
+            }
+            System.out.println();
+        }
+    }
+}
