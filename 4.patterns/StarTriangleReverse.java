4280ecfac816b38e369e09fc22fe4ecada6367f5
om gajera <omgajera8@gmail.com>
2026-05-11 00:46:16 +0530

    Fix output in StarTriangleReverse class to print stars instead of column index

--- 4.patterns/StarTriangleReverse.java
+++ 4.patterns/StarTriangleReverse.java
@@ -8,11 +8,11 @@ public class StarTriangleReverse{
                 
         for(int i = 1; i<= m;i++){
             for(int j = 1; j<= m;j++){
-               if((i+j)<=m){
+               if(i+j)<=m){
                 System.out.print(" ");
                }
                else{
-                System.out.print(j);
+                System.out.print("* ");
                }
 
             }
