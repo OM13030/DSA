4d0e75f735b34a0e6e06e1d34af6ae82f3c8c0cc
om gajera <omgajera8@gmail.com>
2026-06-18 15:59:19 +0530

    Add merge sort implementation with helper methods for array sorting

---
+++ 16. revision/divideandconquer/one.java
@@ -0,0 +1,60 @@
+public class one{
+    public static void printArr(int arr[]){
+        for(int i =0; i<arr.length; i++){
+            System.out.print(arr[i]+ " ");
+        }
+        System.out.println();
+    }
+
+    public static void mergeSort(int arr[], int si , int ei){
+        if(si >= ei){
+            return;
+        }
+
+        // kaam
+        int mid = si + (ei - si)/2;
+        mergeSort(arr, si, mid);
+        mergeSort(arr, mid+1, ei);
+        merge(arr, si, mid, ei);
+
+
+    }
+
+    public static void merge(int arr[],int si,int  mid,int  ei){
+        int temp[] = new int[ei-si+1];
+        int i = si;     // iterator for left part
+        int j = mid + 1;   // iterator for right part
+        int k = 0;     // iterator for temp arr
+
+        while(i <= mid && j <= ei){
+            if(arr[i]< arr[j]){
+                temp[k] = arr[j];
+                i++; 
+            }else{
+                temp[k] = arr[j];
+                j++;
+            }
+            k++;
+        }
+
+        //left part
+        while(i <= mid){
+            temp[k++] = arr[i++];
+        }
+
+        //rigth part
+        while(i <= mid){
+            temp[k++] = arr[j++];
+        }
+
+        for(k=0, i=si; k<temp.length; k++, i++){
+            arr[i] = temp[k];
+            
+        }
+    }
+
+
+    public static void main(String[] args) {
+        int arr[] = {21,3,5,45,65,2,34,7};
+    }
+}
