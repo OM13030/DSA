cf3f8219325fb43044c7bc08410674fa76de6979
om gajera <omgajera8@gmail.com>
2026-06-18 21:07:50 +0530

    Add binary search implementation for sorted array

--- 16. revision/divideandconquer/one.java
+++ 16. revision/divideandconquer/one.java
@@ -87,6 +87,44 @@ public class one{
           
     }
 
+    public static int search(int arr[], int tar, int si , int ei){
+        if(si > ei){
+            return -1;
+        }
+        //kaam
+        int mid = si + (ei - si) / 2;
+
+
+        //case found
+        if(arr[mid] == tar){
+            return mid;
+        }
+
+        //mid on l1
+        if(arr[si] <= arr[mid]){
+            //case a : left
+            if(arr[si] <= tar && tar <= arr[mid]){
+                return search(arr, tar, si, mid);
+            }
+            else{
+                //case B : right
+                search(arr, tar, mid+1, ei);
+            }
+        }
+
+        //mid on L2
+        else{
+            //case c : right
+            if(arr[mid]<= tar && tar <= arr[ei]){
+                return search( arr , tar, mid+1, ei);
+            }
+            else{
+                //case d : left
+                return search(arr, tar, si, mid-1);
+            }
+        }
+    }
+
 
     public static void main(String[] args) {
         int arr[] = {21,3,5,45,65,2,34,7};
