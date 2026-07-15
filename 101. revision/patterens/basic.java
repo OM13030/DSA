83bbf8133fac8641e24655e29127854db04f7241
om gajera <omgajera8@gmail.com>
2026-07-15 18:09:11 +0530

    Enhance basic class with new pattern printing methods and update main method

--- 101. revision/patterens/basic.java
+++ 101. revision/patterens/basic.java
@@ -34,8 +34,57 @@ public class basic{
     //to print the alpha square 
     public static void alphaSquare(int n){
         for(int i = 1;i <= n; i++){
-            for(int j = 1; j <= n; j++){
-                System.out.print(j + " ");
+            for(int j = 1; j <= i; j++){
+                System.out.print((char)(i + 64) + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    // to print even odd pattern
+    public static void Evenoddpyramid(int n){
+        for(int i = 1;i <= n; i++){
+            for(int j = 1; j <= i; j++){
+                if(i % 2 == 0){
+                    System.out.print((char)(j+64) + " ");
+                }else{
+                    System.out.print(j+ " ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+
+    //start triangle ulta
+    public static void StarTriangleUlta(int n){
+        for(int i = 1;i <= n; i++){
+            for(int j = 1; j <= n+1-i; j++){
+                System.out.print("*" + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    // even odd triangle ulta
+    public static void EvenOddtriangleUlta(int n){
+        for(int i = 1;i <= n; i++){
+            for(int j = 1; j <= n+1-i; j++){
+                if(i % 2 == 1){
+                    System.out.print((char)(i+64) + " ");
+                }else{
+                    System.out.print(i+ " ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    //odd number triangle
+    public static void Oddtriangle(int n){
+        for(int i = 1;i <= n; i++){
+            for(int j = 1; j <= i; j++){
+                 System.out.print((2*j)-1 + " ");
             }
             System.out.println();
         }
@@ -45,12 +94,12 @@ public class basic{
     public static void main(String[] args) {
         int n = 4;
         int m = 5;
-        for(int i = 1; i <= n; i++){
-            for(int j = 1;j <= m; j++){
-                System.out.print("* ");
-            }
-            System.out.println();
-        }
-        NumStraightSquare(n);
+        // for(int i = 1; i <= n; i++){
+        //     for(int j = 1;j <= m; j++){
+        //         System.out.print("* ");
+        //     }
+        //     System.out.println();
+        // }
+       Oddtriangle(n);
     }
 }
