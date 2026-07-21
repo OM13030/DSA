00f155ad552a67cb9d4a4355915c1dc2c1b9f510
om gajera <omgajera8@gmail.com>
2026-07-21 15:33:57 +0530

    Add floydTriangle method to basic class and update main method

--- 101. revision/patterens/basic.java
+++ 101. revision/patterens/basic.java
@@ -89,6 +89,16 @@ public class basic{
             System.out.println();
         }
     }
+    public static void floydTriangle(int n){
+        int a = 1;
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= i; j++){
+                System.out.print(a+" ");
+                a++;
+            }
+            System.out.println();
+        }
+    }
 
 
     public static void main(String[] args) {
@@ -100,6 +110,6 @@ public class basic{
         //     }
         //     System.out.println();
         // }
-       Oddtriangle(n);
+       floydTriangle(n);
     }
 }
